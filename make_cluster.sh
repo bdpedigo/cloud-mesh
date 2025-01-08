0d056f09a2b95bc4fb034744ff3703e4492f0ea2
Ben Pedigo <benjamindpedigo@gmail.com>
2025-01-08 10:37:10 -0800

    stash

--- make_cluster.sh
+++ make_cluster.sh
@@ -13,18 +13,18 @@ gcloud config set project exalted-beanbag-334502
 # num-nodes: The number of nodes to be created in each of the cluster's zones.
 # --machine-type "c2d-standard-32" \
 # gcloud container --project "exalted-beanbag-334502" clusters delete "cloud-mesh" --zone "us-west1-b"
-gcloud container --project "exalted-beanbag-334502" clusters create "cloud-mesh-16x16" \
+gcloud container --project "exalted-beanbag-334502" clusters create "cloud-mesh-16x64" \
     --zone "us-west1-b" \
     --no-enable-basic-auth \
     --release-channel "stable" \
-    --machine-type "c2d-standard-16" \
+    --machine-type "c2d-highmem-16" \
     --image-type "COS_CONTAINERD" \
     --disk-type "pd-standard" \
     --disk-size "600" \
     --metadata disable-legacy-endpoints=true \
     --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
     --preemptible \
-    --num-nodes "16" \
+    --num-nodes "64" \
     --logging=SYSTEM,WORKLOAD \
     --monitoring=SYSTEM \
     --enable-ip-alias \
@@ -40,7 +40,7 @@ gcloud container --project "exalted-beanbag-334502" clusters create "cloud-mesh-
     --enable-shielded-nodes \
     --node-locations "us-west1-b"
 
-gcloud container clusters get-credentials --zone us-west1-b cloud-mesh-16x16
+gcloud container clusters get-credentials --zone us-west1-b cloud-mesh-16x64
 
 # https://kubernetes.io/docs/concepts/configuration/secret/
 kubectl create secret generic secrets \
